bd79918ddb561054e8db3886d52ca791b246e4ed
BlackMagic Master <112019925+blackmagic-master@users.noreply.github.com>
2026-05-22 14:05:20 +0300

    Preparation for a new version

--- integrity-check.sh
+++ integrity-check.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+# Default configuration variables
 def_log_init="/var/log"
-hash_file_init="/var/hashes.0"
+hash_file_init="$def_log_init/.hashes"
 
+# Current configuration variables (can be modified by config command)
 def_log=$def_log_init
-hash_file=$hash_file_init
+hash_file="$def_log/.hashes"
 
+# Passed arguments
 ARGS=($1 $2 $3)
 
+# Script path and version
 script=$(realpath $0)
-version="1.0"
+version="1.1"
 
+# Check if the specified path exists
+existance_check(){
+	if [ ! -e "$1" ]; then
+		echo "error: specified path does not exist: $1"
+		exit 10
+	fi
+}
+
+# Error handling for unknown commands
+error(){
+	echo "error: unknown command: $1"
+	echo "use 'help' command for usage information"
+	exit 3
+}
+
+# Check if the script is run as root
 root_check(){
 	if [ "$(whoami)" != "root" ]; then
 		echo "error: this tool must be run as root."
@@ -18,30 +38,42 @@ root_check(){
 	fi
 }
 
+# Initialize the hash file with the specified log directory (or default if not provided)
 init(){
-	if [ -z $1 ]; then
-		echo "Using default log directory: $def_log"
+	if [ ! -z $2 && $2 != "-y" ]; then
+		error $2
 	else
-		echo "Using specified log directory: $1"
-		def_log=$1
-	fi
-	if [ -f $hash_file ]; then
-		echo "Hash file already exists: $hash_file"
-		echo "Do you want to overwrite it? (Y/n)"
-		read answer
-		if [ "$answer" != "Y" ] && [ "$answer" != "y" ]; then
-			echo "Operation cancelled."
-			exit 2
+		if [ -z $1 ]; then
+			echo "Using default log directory: $def_log"
+		else
+			existanse_check $1
+			echo "Using specified log directory: $1"
+			def_log=$1
 		fi
-		find $def_log -type f | xargs sha256sum > $hash_file
-		echo "Hash file created: $hash_file"
-	else
-		find $def_log -type f | xargs sha256sum > $hash_file
-		echo "Hash file created: $hash_file"
+		if [ -f $hash_file ]; then
+			echo "Hash file already exists: $hash_file"
+			if [ $2 != "-y" ]; then
+				echo "Do you want to overwrite it? (Y/n)"
+				read answer
+				if [ "$answer" != "Y" ] && [ "$answer" != "y" ]; then
+					echo "Operation cancelled."
+					exit 2
+				fi
+			fi
+			find $def_log -type f | xargs sha256sum > $hash_file
+			echo "Hash file created: $hash_file"
+		else
+			find $def_log -type f | xargs sha256sum > $hash_file
+			echo "Hash file created: $hash_file"
+		fi
 	fi
 }
 
+# Check the integrity of the specified file against the hash file
 check(){
+	if [ ! -z $1 ]; then
+		existanse_check $1
+	fi
 	files=$(cat $hash_file | grep "$1" | awk '{print $2}')
 	for file in $files; do
 		hash=$(cat $hash_file | grep $file | awk '{print $1}')
@@ -54,23 +86,34 @@ check(){
 	done
 }
 
+# Update the hash file with any new files in the log directory (or default if not provided)
 update(){
-	echo "update"
-	temp_file=$(cat $hash_file)
-	cat /dev/null > $hash_file
-	for line in $temp_file; do
-		if ! grep -q "$line | awk '{print $2}'" $hash_file; then
-			echo "$line" >> $hash_file
-		fi
-	done
+	if [ -z $1 ]; then
+		echo "Using default log directory: $def_log"
+		log_dir=$def_log
+		new_hashes=$(find $log_dir -type f | xargs sha256sum)
+		temp_file=$(cat $hash_file)
+		cat /dev/null > $hash_file
+		for line in $temp_file; do
+			if ! grep -q "$line | awk '{print $2}'" $hash_file; then
+				echo "$line" >> $hash_file
+			fi
+		done
+	else
+		existanse_check $1
+		log_dir=$1
+	fi
 }
 
+# Display help message with usage information
 help(){
 	echo "Log integrity check tool v.$version"
 	echo "usage: integrity-check [command] file [argument]"
 	echo "note:  this tool requires superuser privileges"
 	echo "commands:"
-	echo "  init [log_directory] - initialize the hash file with the specified log directory (default: /var/log)"
+	echo "  init [log_directory] [-y] - initialize the hash file with the specified log directory (default: /var/log)"
+	echo "		-y - skip confirmation prompt when overwriting existing hash file"
+	echo "		to use -y option, the log_directory argument must be provided (can be default)"
 	echo "  check file - check the integrity of the specified file"
 	echo "  update - update the hash file with any new files in the log directory"
 	echo "  help - display this help message"
@@ -81,6 +124,7 @@ help(){
 	echo "   	 show - display current and default configuration"
 }
 
+# Configure the tool with the specified option and value
 config(){
 	case $1 in
 		"log")
@@ -114,39 +158,39 @@ config(){
 	esac
 }
 
-error(){
-	echo "error: unknown command: $1"
-	echo "use 'help' command for usage information"
-	exit 3
-}
-
-main(){
-	root_check
+# Check if any arguments were provided
+args_check(){
 	if [ -z $ARGS ]; then
 		echo "No arguments given"
 		echo "use 'help' command for usage information"
 		exit 4
-	else
-		case ${ARGS[0]} in
-			"init")
-				init ${ARGS[1]}
-				;;
-			"check")
-				check ${ARGS[1]}
-				;;
-			"update")
-				update
-				;;
-			"help")
-				help
-				;;
-			"config")
-				config ${ARGS[1]} ${ARGS[2]}
-				;;
-			*)
-				error ${ARGS[0]}
-		esac
 	fi
 }
 
+# Main function
+main(){
+	root_check
+	args_check
+	case ${ARGS[0]} in
+		"init")
+			init ${ARGS[1]} ${ARGS[2]}
+			;;
+		"check")
+			check ${ARGS[1]}
+			;;
+		"update")
+			update ${ARGS[1]}
+			;;
+		"help")
+			help
+			;;
+		"config")
+			config ${ARGS[1]} ${ARGS[2]}
+			;;
+		*)
+			error ${ARGS[0]}
+	esac
+}
+
+# Running the script
 main
